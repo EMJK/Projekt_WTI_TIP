3c66e672774e9f05e61c6d30bb556cd15c72c68c
Julian Kwieciński <julkwiec@gmail.com>
2016-06-29 21:52:59 +0200

    Porządki

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,7 +8,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
-using Audio;
 using Julas.Utils;
 using Microsoft.AspNet.SignalR.Client;
 using ChatClient;
@@ -21,17 +20,7 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            //var recorder = new AudioRecorder();
-            //var stream = recorder.GetAudioPacketStream(0, 8000/50);
-            //var player = new AudioPlayer();
-            //var playHandle = player.PlayAudioPacketStream(0, stream.PacketSource.Delay(TimeSpan.FromSeconds(1)));
-            //Thread.Sleep(10000000);
-                
-            //return;
-            Task.WaitAll(
-                Enumerable.Range(1, 2)
-                    .Select(x => Task.Factory.StartNew(() => Application.Run(new MainForm())))
-                    .ToArray());
+            Application.Run(new MainForm());
         }
     }
 }
